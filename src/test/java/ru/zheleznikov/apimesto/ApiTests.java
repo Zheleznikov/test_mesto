f67b56b294de68989c484c6097b55bf4e574f0bc
Serg <polwen@yandex.ru>
2021-05-29 22:52:22 +0300

    Add var data in BaseTests

--- src/test/java/ru/zheleznikov/apimesto/ApiTests.java
+++ src/test/java/ru/zheleznikov/apimesto/ApiTests.java
@@ -2,6 +2,7 @@ package ru.zheleznikov.apimesto;
 
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+import org.testng.Assert;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -9,39 +10,36 @@ import java.util.Map;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
-public class ApiTests {
+public class ApiTests extends BaseApiTests {
 
-    String apiHost = "https://api-mesto.zheleznikov.ru/";
 
     @Test
     public void signinTest() {
-        Map<String, String> requestData = new HashMap<>();
-        requestData.put("email", "polwen@yandex.ru");
-        requestData.put("password", "1234qwerty");
-
         Response response = given()
                 .contentType("application/json")
-                .body(requestData)
+                .body(correctRequestData)
                 .when()
-                .post(apiHost + "signin")
+                .post(apiHost + signin)
                 .then()
-//                .log().body()
+                .log().body()
                 .extract()
                 .response();
 
+
         String token = "Bearer " + response.jsonPath().get("token");
         System.out.println(token);
+        String email = response.jsonPath().get("user.email");
+        System.out.println(email);
 
         // get info about me
 
-        given()
-                .contentType("application/json")
-                .header("Authorization", token)
-                .when()
-                .get(apiHost + "me")
-                .then()
-                .log().all();
-
+//        given()
+//                .contentType("application/json")
+//                .header("Authorization", token)
+//                .when()
+//                .get(apiHost + "me")
+//                .then()
+//                .log().all();
 
     }
 
--- src/test/java/ru/zheleznikov/apimesto/BaseApiTests.java
+++ src/test/java/ru/zheleznikov/apimesto/BaseApiTests.java
@@ -4,15 +4,28 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class BaseApiTests {
+    // apiHost
     protected final static String apiHost = "https://api-mesto.zheleznikov.ru/";
-    protected final static String signin = "/signin";
+    protected final static String signin = "signin";
 
-    protected static Map<String, String> correctRequestData = new HashMap<>();
+    // signin Data
+    protected final static Map<String, String> correctRequestData = new HashMap<>();
+    protected final static Map<String, String> incorrectDataEmail = new HashMap<>();
+    protected final static Map<String, String> incorrectDataPass = new HashMap<>();
+
+    protected final static String emailKey = "email";
+    protected final static String passKey = "password";
+
+    protected final static String correctEmail = "cat@cat.cat";
+    protected final static String correctPass = "1234qwerty";
+    protected final static String incorrectPass = "1234qwert";
+    protected final static String emptyField = "";
+    protected final static String incorrectEmail = "mail@";
+
 
     static {
-        correctRequestData.put("email", "polwen@yandex.ru");
-        correctRequestData.put("password", "1234qwerty");
-
+        correctRequestData.put(emailKey, correctEmail);
+        correctRequestData.put(passKey, correctPass);
     }
 
 }
