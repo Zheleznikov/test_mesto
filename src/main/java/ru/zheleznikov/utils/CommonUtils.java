1bda118eeef59d334592bd2c3ac181562e4627c6
Serg <polwen@yandex.ru>
2021-06-02 20:15:35 +0300

    Add second method how to put request in body Add post card Try to delete file

--- src/main/java/ru/zheleznikov/utils/CommonUtils.java
+++ src/main/java/ru/zheleznikov/utils/CommonUtils.java
@@ -9,6 +9,12 @@ import java.util.Scanner;
 
 public class CommonUtils {
 
+    /**
+     *     1 способ. Получение содержимого файла, как объект с типом данных JSONObject
+     *     Для дальнейшего преобразования в класс с валидными параметрами.
+     *     После этого класс передается в тело запроса
+     *     с помощью метода getSigninJson
+      */
     private static JSONObject getJson(String path) throws IOException {
         Scanner sc = new Scanner(new File(path));
         String jsonFile = "";
@@ -18,8 +24,30 @@ public class CommonUtils {
         return new JSONObject(jsonFile);
     }
 
+    /**
+     * 2 способ. Получение содержимого файла, как объект с типом данных String
+     * После этого полученная строка сразу же передается как строка в тело запроса
+     */
+    private static String getFileContentAsString(String path) throws IOException {
+        Scanner sc = new Scanner(new File(path));
+        String file = "";
+        while (sc.hasNextLine()) {
+            file += sc.nextLine();
+        }
+        return file;
+    }
+
     public static Signin getSigninJson() throws IOException {
         JSONObject obj = getJson("src/main/resources/signin.json");
         return new Signin(obj.getString("email"), obj.getString("password"));
+    }
+
+    public static String getSigninJson2() throws IOException {
+        return getFileContentAsString("src/main/resources/signin.json");
+    }
+
+    public static String postCardJson() throws IOException {
+        return getFileContentAsString("src/main/resources/postCard.json");
+
     }
 }
--- src/test/java/ru/zheleznikov/apimesto/ApiTests.java
+++ src/test/java/ru/zheleznikov/apimesto/ApiTests.java
@@ -15,7 +15,7 @@ import java.util.Scanner;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 import static ru.zheleznikov.apimesto.Specification.reqSpec;
-import static ru.zheleznikov.utils.CommonUtils.getSigninJson;
+import static ru.zheleznikov.utils.CommonUtils.*;
 
 import org.json.*;
 import ru.zheleznikov.jsonClasses.Signin;
@@ -26,9 +26,10 @@ public class ApiTests extends BaseApiTests {
     @Description("1. /signin - positive signing check")
     public void signinTestPositive() throws IOException {
         Signin apiSignin = getSigninJson();
+        String apiSignin2 = getSigninJson2();
 
         given()
-                .spec(reqSpec(apiSignin))
+                .spec(reqSpec(apiSignin2))
                 .when()
                 .post(signin)
                 .then()
@@ -39,9 +40,6 @@ public class ApiTests extends BaseApiTests {
                 .body("token", notNullValue());
     }
 
-
-
-
     @Test
     public void getMyData() {
         String token = getToken();
@@ -51,18 +49,47 @@ public class ApiTests extends BaseApiTests {
                 .when()
                 .get(me)
                 .then()
-                .log().all();
+                .log().body();
     }
 
     @Test
     public void getCards() {
-        given()
+        String id = given()
                 .spec(reqSpec(""))
                 .when()
                 .get("cards")
                 .then()
-                .log().headers();
+//                .log().body()
+                .extract().path("data.owner").toString();
+        System.out.println(id);
     }
 
+    @Test
+    public void postCard() throws IOException {
+        String token = getToken();
+        String postCard = postCardJson();
+        given()
+                .spec(reqSpec(postCard))
+                .header("authorization", token)
+                .when()
+                .post("cards")
+                .then()
+                .log().all();
+    }
+
+    @Test
+    public void deleteCard() {
+        String token = getToken();
+        String id = "60b7b9b1ec264003d4e93321";
+
+        given()
+                .spec(reqSpec(""))
+                .header("authorization", token)
+                .when()
+                .delete("cards/" + id)
+                .then()
+                .log().all();
+
+    }
 
 }
