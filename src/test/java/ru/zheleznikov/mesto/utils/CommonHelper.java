b794ad46eb41c03f636b00e85639d7284d4d8f0b
Serg <polwen@yandex.ru>
2021-07-13 21:44:06 +0300

    Add method to generate random names and places

--- src/test/java/ru/zheleznikov/mesto/utils/CommonHelper.java
+++ src/test/java/ru/zheleznikov/mesto/utils/CommonHelper.java
@@ -2,11 +2,46 @@ package ru.zheleznikov.mesto.utils;
 
 import ru.zheleznikov.mesto.model.Card;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CommonHelper {
 
     public static Card getRandomCard(List<Card> cards) {
-        return cards.get((int) ( Math.random() * cards.size()));
+        return cards.get((int) (Math.random() * cards.size()));
     }
+
+    public static String getRandomLink() throws IOException {
+        List<String> links = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/links.txt"))) {
+            String line = reader.readLine();
+            while (line != null) {
+                links.add(line);
+                line = reader.readLine();
+            }
+            return links.get((int) (Math.random() * links.size()));
+        }
+
+
+    }
+
+    public static String getRandomName() throws IOException {
+        List<String> links = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/names.txt"))) {
+            String line = reader.readLine();
+            while (line != null) {
+                links.add(line);
+                line = reader.readLine();
+            }
+            return links.get((int) (Math.random() * links.size()));
+        }
+    }
+
+
+
 }
