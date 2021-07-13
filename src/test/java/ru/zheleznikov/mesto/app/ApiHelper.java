bf49348263ff9c65bd4cd982d452222c94dbf3f0
Serg <polwen@yandex.ru>
2021-07-13 19:44:42 +0300

    Add apiHelper

---
+++ src/test/java/ru/zheleznikov/mesto/app/ApiHelper.java
@@ -0,0 +1,38 @@
+package ru.zheleznikov.mesto.app;
+
+import io.restassured.response.Response;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ApiHelper extends ApiHelperBase {
+
+
+    public ApiHelper() throws IOException {
+
+    }
+
+    private List<Object> processResponse(Response res) {
+        return res.then()
+                .statusCode(200)
+                .extract().jsonPath().getList("data");
+    }
+
+    public List<Object> getCards() {
+        return processResponse(reqGetCards());
+    }
+
+    public String addCard(String body) {
+        Response res = reqPostCard(body);
+        return res.then().log().body().extract().path("data._id");
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ src/test/java/ru/zheleznikov/mesto/app/ApiHelperBase.java
@@ -0,0 +1,62 @@
+package ru.zheleznikov.mesto.app;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import ru.zheleznikov.mesto.model.Signin;
+
+import java.io.IOException;
+
+import static io.restassured.RestAssured.given;
+
+public class ApiHelperBase extends HelperBase {
+
+    private final String HOST = properties.getProperty("host.api");
+    private final String EMAIL = properties.getProperty("email");
+    private final String PASSWORD = properties.getProperty("password");
+    private final Signin SIGNIN = new Signin().withEmail(EMAIL).withPassword(PASSWORD);
+
+
+    public ApiHelperBase() throws IOException {
+    }
+
+    protected Response reqGetCards() {
+        return given()
+                .spec(reqSpec())
+                .when()
+                .get("cards");
+    }
+
+    protected Response reqPostSignin() {
+        return given()
+                .spec(reqSpec())
+                .body(SIGNIN)
+                .when()
+                .post("signin");
+    }
+
+    protected Response reqPostCard(String body) {
+        return given()
+                .spec(reqSpec())
+                .header("authorization", getToken())
+                .body(body)
+                .when()
+                .post("cards");
+    }
+
+    private String getToken() {
+        Response res = reqPostSignin();
+        return "Bearer " + res
+                .then()
+                .extract()
+                .path("token");
+    }
+
+    private RequestSpecification reqSpec() {
+        return new RequestSpecBuilder()
+                .setBaseUri(HOST)
+                .setContentType(ContentType.JSON)
+                .build();
+    }
+}
